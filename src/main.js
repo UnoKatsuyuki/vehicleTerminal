0d5fbdbf09640c938b1274d3bafccee95c01e7bd
一品玩家 <firstplayer1208@gmail.com>
2025-07-03 14:52:41 +0800

    接入了设置界面，优化了界面逻辑

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import { createVuestic } from 'vuestic-ui'
+import 'vuestic-ui/css'
 
 import App from './App.vue'
 import router from './router'
@@ -13,5 +15,6 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(ElementPlus)
+app.use(createVuestic())
 
 app.mount('#app')
